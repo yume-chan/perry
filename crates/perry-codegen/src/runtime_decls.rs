f7ea14eae786a1ea86e13ad9df71f6b446e59525
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2026-04-23 16:57:06 +0800

    fix: add back missing runtime declaration

--- crates/perry-codegen/src/runtime_decls.rs
+++ crates/perry-codegen/src/runtime_decls.rs
@@ -187,7 +187,9 @@ pub fn declare_phase_b_strings(module: &mut LlModule) {
     module.declare_function("js_math_max_array", DOUBLE, &[I64]);
     module.declare_function("js_string_coerce", I64, &[DOUBLE]);
     module.declare_function("js_array_slice", I64, &[I64, I32, I32]);
-    module.declare_function("js_array_is_array", DOUBLE, &[DOUBLE]);    module.declare_function("js_set_alloc", I64, &[I32]);
+    module.declare_function("js_array_is_array", DOUBLE, &[DOUBLE]);
+    module.declare_function("js_array_shift_f64", DOUBLE, &[I64]);
+    module.declare_function("js_set_alloc", I64, &[I32]);
     module.declare_function("js_set_from_array", I64, &[I64]);
     module.declare_function("js_map_from_array", I64, &[I64]);
     module.declare_function("js_object_has_property", DOUBLE, &[DOUBLE, DOUBLE]);
